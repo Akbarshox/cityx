2e01cfa5e78fa3ba49206d37c82a7302a204899a
Akbarshox <akbarshax@bk.ru>
2020-02-26 16:32:56 +0500

    preloader

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React, {useState, useEffect} from 'react';
+import Routes from "./Routes";
+import { css } from "@emotion/core";
+import { BarLoader } from "react-spinners";
+
+const override = css`
+  position: absolute;
+  display: flex;
+  position: fixed;
+  z-index: 1031;
+  top: 50%;
+  left: 25%;
+`;
+
+export default function App() {
+   const [isLoading, setLoading] = useState(true);
+   
+   useEffect(() => {
+      const timer = setTimeout(() => {
+         setLoading(false)
+      }, 4000);
+      return () => clearTimeout(timer);
+   }, []);
+   
+   if(isLoading === true){
+   return(
+       <div>
+          <p className="loading">Loading...</p>
+          <BarLoader
+              css={override}
+              //size={"150px"} this also works
+              color={"#00f7ce"}
+              loading={isLoading}
+              height="2"
+              width="50%"
+          />
+       </div>
+   )}else{
+      return <Routes />
+   }
+}
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,65 +1,11 @@
 import React from 'react';
-import Particles from "react-particles-js";
+import ParticlesBg from 'particles-bg'
 
 export default function Contact() {
+   let arr = ["color", "cobweb", "polygon"];
    return(
        <div className="container">
-          <Particles
-              params={{
-                 "particles": {
-                    "number": {
-                       "value": 8,
-                       "density": {
-                          "enable": true,
-                          "value_area": 800
-                       }
-                    },
-                    "line_linked": {
-                       "enable": false
-                    },
-                    "move": {
-                       "speed": 1,
-                       "out_mode": "out"
-                    },
-                    "shape": {
-                       "type": [
-                          "images",
-                          "circle"
-                       ],
-                       "images": [
-                          {
-                             "src": "../assets/Instagram.svg",
-                             "height": 20,
-                             "width": 23
-                          },
-                          {
-                             "src": "../assets/Mail.svg",
-                             "height": 20,
-                             "width": 20
-                          },
-                          {
-                             "src": "../assets/Github.svg",
-                             "height": 20,
-                             "width": 20
-                          }
-                       ]
-                    },
-                    "color": {
-                       "value": "#CCC"
-                    },
-                    "size": {
-                       "value": 30,
-                       "random": false,
-                       "anim": {
-                          "enable": true,
-                          "speed": 4,
-                          "size_min": 10,
-                          "sync": false
-                       }
-                    }
-                 },
-                 "retina_detect": false
-              }} />
+          <ParticlesBg type={arr[Math.floor(Math.random()*arr.length)]} bg={true} color="#00f7ce" />
        </div>
    )
 }
